4bba920daa63562c0728b4051775489f5dc542aa
zhengtongxue <209665530@qq.com>
2019-11-24 10:26:48 +0800

    基于一行只有一个点的原则将方法继续迁移到新类中

--- Shops/Movie.cs
+++ Shops/Movie.cs
@@ -15,5 +15,39 @@
         public string Title { get; }
 
         public int PriceCode { get; set; }
+
+        public double CountThisAmount(int dayRented)
+        {
+            double thisAmount = 0;
+
+            switch (this.PriceCode)
+            {
+                case Movie.Regular:
+                {
+                    thisAmount += 2;
+                    if (dayRented > 2)
+                    {
+                        thisAmount += (dayRented - 2) * 1.5;
+                    }
+                }
+                    break;
+                case Movie.NewRelease:
+                {
+                    thisAmount += dayRented * 3;
+                }
+                    break;
+                case Movie.Children:
+                {
+                    thisAmount += 1.5;
+                    if (dayRented > 3)
+                    {
+                        thisAmount += (dayRented - 3) * 1.5;
+                    }
+                }
+                    break;
+            }
+
+            return thisAmount;
+        }
     }
 }
--- Shops/Rental.cs
+++ Shops/Rental.cs
@@ -14,43 +14,7 @@
 
         public double CountThisAmount()
         {
-            var dayRented = DayRented;
-
-            return CountThisAmount(dayRented);
-        }
-
-        private double CountThisAmount(int dayRented)
-        {
-            double thisAmount = 0;
-
-            switch (Movie.PriceCode)
-            {
-                case Movie.Regular:
-                {
-                    thisAmount += 2;
-                    if (dayRented > 2)
-                    {
-                        thisAmount += (dayRented - 2) * 1.5;
-                    }
-                }
-                    break;
-                case Movie.NewRelease:
-                {
-                    thisAmount += dayRented * 3;
-                }
-                    break;
-                case Movie.Children:
-                {
-                    thisAmount += 1.5;
-                    if (dayRented > 3)
-                    {
-                        thisAmount += (dayRented - 3) * 1.5;
-                    }
-                }
-                    break;
-            }
-
-            return thisAmount;
+            return Movie.CountThisAmount(DayRented);
         }
 
         public string CountDetail()
